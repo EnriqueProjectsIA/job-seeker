305cc04967e987485ca5864f64a22862465a8562
Enrique Vilanova <envivi@gmail.com>
2023-02-19 21:51:29 +0100

    Añado apertura de navegador

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Project description
 
 The "Job-seeker" project has a main goal of helping people in their job search and at the same time, examining the trends in the skills and tools required for a particular job position. With an innovative approach, this project aims to simplify the job search process and provide valuable information about labor market trends.
+
+## Installation
+
++ After installing the requirements may be necessary execute the command line “playwright install”
--- web-scraper/linkedin.py
+++ web-scraper/linkedin.py
@@ -1,2 +1,30 @@
 import pandas as pd
 from bs4 import BeautifulSoup as bs
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.linkedin.com/")
+    page.get_by_role("link", name="Sign in").click()
+    page.get_by_label("Email or Phone").click()
+    page.get_by_label("Email or Phone").fill("envivi@gmail.com")
+    page.get_by_label("Password").click()
+    page.get_by_label("Password").fill("Co4cuvaz123@")
+    page.get_by_role("button", name="Sign in", exact=True).click()
+    page.get_by_role("link", name="Jobs").click()
+    page.get_by_role("combobox", name="Search by title, skill, or company").click()
+    page.get_by_role("combobox", name="Search by title, skill, or company").fill("Data Scientist")
+    page.get_by_role("combobox", name="City, state, or zip code").click()
+    page.get_by_role("combobox", name="City, state, or zip code").fill("Barcelona, Catalonia, Spain")
+    page.get_by_role("combobox", name="City, state, or zip code").press("Enter")
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
