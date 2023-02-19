f4509a814ac3772132bae635580d1ca0da53c9b4
Enrique Vilanova <envivi@gmail.com>
2023-02-19 22:14:28 +0100

    Elimino bug

--- web-scraper/linkedin.py
+++ web-scraper/linkedin.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import os.path
 from bs4 import BeautifulSoup as bs
 from playwright.sync_api import Playwright, sync_playwright, expect
 
+with open(os.path.dirname(__file__) +'/../secrets.txt', 'r') as f:
+    secret = f.read().splitlines()
+    user = secret[0].strip().split(':')[1]
+    password = secret[1].strip().split(':')[1]
 
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False, slow_mo=1000)
@@ -10,9 +15,9 @@ def run(playwright: Playwright) -> None:
     page.goto("https://www.linkedin.com/")
     page.get_by_role("link", name="Sign in").click()
     page.get_by_label("Email or Phone").click()
-    page.get_by_label("Email or Phone").fill("envivi@gmail.com")
+    page.get_by_label("Email or Phone").fill(f"{user}")
     page.get_by_label("Password").click()
-    page.get_by_label("Password").fill("Co4cuvaz123@")
+    page.get_by_label("Password").fill(f"{password}")
     page.get_by_role("button", name="Sign in", exact=True).click()
     page.get_by_role("link", name="Jobs").click()
     page.get_by_role("combobox", name="Search by title, skill, or company").click()
@@ -25,6 +30,9 @@ def run(playwright: Playwright) -> None:
     context.close()
     browser.close()
 
+if __name__ == "__main__":
 
-with sync_playwright() as playwright:
-    run(playwright)
+
+    with sync_playwright() as playwright:
+        run(playwright)
+
